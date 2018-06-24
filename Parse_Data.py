a3778487047b0e7f3d2ceb68adbba9dfaee0ab83
Scott Glenn <scottglenn1800@yahoo.com>
2018-06-23 21:33:50 -0400

    *Created CreateStepExcel function that will take all of the Records gathered and log the step, date, start time, and end time into an excel table. *Changed the date time times in GetStepRecords. date will now reflect the date of steps instead of the date of the creation of the record. time will now displayt HH:MM:SS instead of having the date prepended to it.

--- Parse_Data.py
+++ Parse_Data.py
@@ -1,9 +1,9 @@
 #Apple Health Export Parser
-import re,sys,openpyxl,os,distutils.dir_util
+import re,sys,openpyxl,os,distutils.dir_util,datetime
 from optparse import OptionParser
 sys.path.append("C:\CODE\Apple_Health_Export_Parser")
 from Record_Classes import *
-
+date = str(datetime.datetime.now().date()) #Gets Todays date
 def ParseFilename():
 	parser = OptionParser()
 	parser.add_option("-f","--file",type="string",dest="filepath",help="The filepath of the export.xml file to parse")
@@ -36,10 +36,10 @@ def GetStepRecords(entries):
 			steps = re.search(r'[0-9]+',steps.group(0))
 			steps = steps.group(0)
 			#Gets the date out of the entry (YYYY-MM-DD)
-			date = re.search(r'[0-9]{4}-[0-9]{2}-[0-9]{2}',entry)
-			date = date.group(0)
+			date = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}',entry)
+			date = date[1]
 			#Gets the start and end times
-			times = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}',entry)
+			times = re.findall(r'[0-9]{2}:[0-9]{2}:[0-9]{2}',entry)
 			del times[0] #The first time in the record is the record creation date, (doesn't happen at time of event) so it gets deleted
 			starttime = times[0]
 			endtime = times[1]
@@ -48,15 +48,29 @@ def GetStepRecords(entries):
 	return StepRecords
 
 def CreateStepExcel(StepRecords):
-	distutils.dir_util.mkpath() #Creates the Directory. 
-	Sheet = openpyxl.Workbook()
+	filename = "Apple-Export-%s"%(date)
+	distutils.dir_util.mkpath(os.getcwd() + "\Excel_Sheets") #Creates the Directory if it does not already exist
+	WorkBook = openpyxl.Workbook()
+	Sheet = WorkBook.create_sheet() #Creates a new excel sheet 
+	#Creating Headers for Each column
+	Sheet['A1'] = "Steps"
+	Sheet['B1'] = "Date"
+	Sheet['C1'] = "Start Time"
+	Sheet['D1'] = "End Time"
+	row_counter = 2
+	for record in StepRecords:
+		Sheet['A%s'%(row_counter)] = int(record.steps)
+		Sheet['B%s'%(row_counter)] = str(record.date)
+		Sheet['C%s'%(row_counter)] = str(record.starttime)
+		Sheet['D%s'%(row_counter)] = str(record.endtime)
+		row_counter+=1 #moves down a row in the worksheet
+	print"Saving Workbook..."
+	WorkBook.save("StepCount-%s.xlsx"%date)
 
 
 file = ParseFilename()
 entries = GetEntries(file)
 StepRecords = GetStepRecords(entries)
-for record in StepRecords:
-	print record.starttime
-print len(StepRecords)
+CreateStepExcel(StepRecords)
 
 	
